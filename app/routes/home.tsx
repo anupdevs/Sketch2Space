473d5b92c2be19955599abd473bc576f5e445d83
Anup <anupc9605@gmail.com>
2026-07-04 19:09:39 +0530

    Home page UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import { ArrowRight, ArrowUpRight, Clock, Layers, Upload } from "lucide-react";
+import Button from "../../components/ui/Button";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,10 +11,113 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return(
-     <div className="home">
-        <Navbar />
-        <h1 className="text-red-700">Home</h1>
-     </div>
+  return (
+    <div className="home">
+      <Navbar />
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Sketch2Space</p>
+        </div>
+        <h1>Build beautiful spaces at the speed of thought with Sketch2Space</h1>
+
+        <p className="subtitle">
+          Sketch2Space is an AI-first design environment that helps you visualize, render, and ship architectural projects faster  than ever.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+
+            {/* <Upload onComplete={handleUploadComplete} /> */}
+            <Button className="cta">Upload Image</Button>
+          </div>
+        </div>
+      </section>
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared community projects, all in one place.</p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project" />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date('1st Jan 2027').toLocaleDateString()}</span>
+                    <span>By Anup C</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+
+              </div>
+            </div>
+            {/* {projects.map(({id, name, renderedImage, sourceImage, timestamp}) => (
+                          <div key={id} className="project-card group" onClick={() => navigate(`/visualizer/${id}`)}>
+                              <div className="preview">
+                                  <img  src={renderedImage || sourceImage} alt="Project"
+                                  />
+
+                                  <div className="badge">
+                                      <span>Community</span>
+                                  </div>
+                              </div>
+
+                              <div className="card-body">
+                                  <div>
+                                      <h3>{name}</h3>
+
+                                      <div className="meta">
+                                          <Clock size={12} />
+                                          <span>{new Date(timestamp).toLocaleDateString()}</span>
+                                          <span>By JS Mastery</span>
+                                      </div>
+                                  </div>
+                                  <div className="arrow">
+                                      <ArrowUpRight size={18} />
+                                  </div>
+                              </div>
+                          </div>
+                      ))} */}
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
